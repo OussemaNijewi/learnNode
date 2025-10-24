932306060f193ef6dc4b2c84a078a6c82b54a352
Oussema Nijewi <oussemasn@gmail.com>
2025-10-24 18:54:00 -0400

    simple routing example

--- server.js
+++ server.js
@@ -9,8 +9,21 @@ const server = http.createServer((req, res) => {
     //set response packet header content type
     res.setHeader('Content-Type', 'text/html');
 
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
     //send html file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
