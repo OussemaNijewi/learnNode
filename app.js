1abac9405f606a330b84488f0a1608cb7723dc70
Oussema Nijewi <oussemasn@gmail.com>
2025-10-25 17:09:45 -0400

    mongodb setup

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 
+
 //express app
 const app = express();
+
+//connect to mongodb
+const dbURI = 'mongodb+srv://OussemaNijewi:Wess1234@nodetutorial.9hs1i0t.mongodb.net/?appName=NodeTutorial';
 
 //register view engine
 app.set('view engine', 'ejs');
