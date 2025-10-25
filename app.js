79e8bcbaebc13456d0b3d0530a32731d71752cd2
Oussema Nijewi <oussemasn@gmail.com>
2025-10-25 15:39:51 -0400

    passing data to ejs

--- app.js
+++ app.js
@@ -11,19 +11,24 @@ app.listen(3000);
 
 // respond to get request
 app.get('/', (req, res) => {
+    const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
     //res.send('<p>home page</p>');
-    res.render('index');
+    res.render('index', {title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about',{title: 'About'});
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'Create'});
 });
 
 //404 page should be at end of the code
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {title: '404'});
 });
