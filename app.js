e132e4b8ea1dae3b11764571a871a8aa7c9f09ab
Oussema Nijewi <oussemasn@gmail.com>
2025-10-25 21:35:49 -0400

    mvc but it doesn't work :(

--- app.js
+++ app.js
@@ -3,38 +3,41 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogRoutes');
 
-
-//express app
+// express app
 const app = express();
 
-//connect to mongodb
+// connect to mongodb & listen for requests
 const dbURI = 'mongodb+srv://OussemaNijewi:Wess1234@nodetutorial.9hs1i0t.mongodb.net/node-tuts?appName=NodeTutorial';
 
-mongoose.connect(dbURI)
-    .then((result) => app.listen(3000)) //listen for requests only after db connection complete
-    .catch((err) => console.log(err));
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
-//register view engine
+// register view engine
 app.set('view engine', 'ejs');
 
-// middleware and static files
+// middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: true}));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
-// respond to get request
+// routes
 app.get('/', (req, res) => {
-    res.redirect('/blogs');
+  res.redirect('/blogs');
 });
 
-//blog routes
 app.get('/about', (req, res) => {
-    res.render('about',{title: 'About'});
+  res.render('about', { title: 'About' });
 });
 
-app.use('/blogs',blogRoutes);
+// blog routes
+app.use('/blogs', blogRoutes);
 
-//404 page should be at end of the code
+// 404 page
 app.use((req, res) => {
-    res.status(404).render('404', {title: '404'});
+  res.status(404).render('404', { title: '404' });
 });
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,54 +1,18 @@
 const express = require('express');
-const Blog = require('../models/blog');
+const blogController = ('../controllers/blogController');
 
 const router = express.Router();
 
-router.get('/create', (req, res) => {
-    res.render('create', {title: 'Create'});
-});
 
-router.get('/', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-    .then(result => {
-      res.render('index', { blogs: result, title: 'All blogs' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
 
-router.post('/', (req, res) => {
-    const blog = new Blog(req.body);
+router.get('/', blogController.blog_index);
 
-    blog.save()
-        .then((result) =>{
-            res.redirect('/blogs');
-        })
-        .catch((err)=>{
-            console.log(err);
-        });
-});
+router.post('/', blogController.blog_create_post);
 
-router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    Blog.findById(id)
-    .then((result) => {
-        res.render('details', {blog: result, title: 'Blog Details'});
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-});
+router.get('/create', blogController.blog_create_get);
 
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
+router.get('/:id', blogController.blog_details);
 
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({redirect: '/blogs'});
-        })
-        .catch(err => console.log(err));
-});
+router.delete('/:id', blogController.blog_delete);
 
 module.exports = router;
